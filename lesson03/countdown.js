93ed28248430dc32f10763f1bb9aef810b82c34c
kraptastic_k <Kyle Woodburn>
2024-05-23 11:12:27 -0700

    created countdown.js to show proficiency with basic for loop

---
+++ lesson03/countdown.js
@@ -0,0 +1,8 @@
+
+let theVar = 13;
+
+console.log('The initial value of the variable is shown at right: ' + theVar)
+for (let i = 13; i > 0; i-- ){
+  theVar--;
+  console.log('Variable shown at right, decreasing by one: ' + theVar);
+}
