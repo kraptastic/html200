94d4d1837bc89151dbbb282666848eda2ce2eaa3
kraptastic_k <Kyle Woodburn>
2024-06-02 07:12:55 -0700

    added exercise-one.js on new branch

---
+++ lesson04/exercise-one.js
@@ -0,0 +1,8 @@
+
+let theArray = ['animal', 'vegetable', 'mineral', 'unclassified', 'conceptual'];
+console.log(theArray);
+theArray.splice(4, 0, 'mystical');
+console.log(theArray);
+theArray.splice(2,1,);
+console.log(theArray);
+console.log(theArray.join(' , '));
